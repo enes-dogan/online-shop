e56693204e5b9f4875dff485b2eea5cd8921c2fe
Enes Dogan <enesdogan210@gmail.com>
2023-03-16 19:25:14 +0300

    login authentication using sessions and few new routes added

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const path = require('path');
 
 const express = require('express');
+const csrf = require('csurf');
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const db = require('./data/database');
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error.handler');
 const authRoutes = require('./routes/auth.routes');
+const productsRoutes = require('./routes/products.routes');
+const baseRoutes = require('./routes/base.routes');
+
 
 const app = express();
 
@@ -13,7 +21,19 @@ app.set('views', path.join(__dirname, 'views')); // path views is folder contain
 app.use(express.static('public')); // public folder serves staticly
 app.use(express.urlencoded({ extended: false })); // for handling data attached to request
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csrf());
+
+app.use(addCsrfTokenMiddleware);
+
+app.use(baseRoutes);
 app.use(authRoutes);
+app.use(productsRoutes);
+
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
   .then(function () {
