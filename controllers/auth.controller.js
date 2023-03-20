bd5132ddaa8ab552d35ffb3388309d2975348762
Enes-Dogan <enesdogan210@gmail.com>
2023-03-20 23:49:02 +0300

    validating user input and comparing emails

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,27 @@
 const User = require('../models/user.model');
 const authUtil = require('../util/authentication');
+const validation = require('../util/validation');
 
 function getSignup(req, res) {
   res.render('customer/auth/signup');
 }
 
-async function signup(req, res) {
+async function signup(req, res, next) {
+  if (
+    !validation.userDetailsAreValid(
+      req.body.email,
+      req.body.password,
+      req.body.fullname,
+      req.body.street,
+      req.body.postal,
+      req.body.city
+    ) ||
+    !validation.emailIsConfirmed(req.body.email, req.body['confirm-email'])
+  ) {
+    res.redirect('/signup');
+    return;
+  }
+
   const user = new User(
     req.body.email,
     req.body.password,
@@ -15,7 +31,19 @@ async function signup(req, res) {
     req.body.city
   );
 
-  await user.signup();
+  try {
+    const existsAlready = await user.existAlready();
+
+    if (existsAlready) {
+      res.redirect('/signup');
+      return;
+    }
+
+    await user.signup();
+  } catch (error) {
+    next(error);
+    return;
+  }
 
   res.redirect('/login');
   // Controller functions that are triggered by incoming post requests,
@@ -27,9 +55,15 @@ function getLogin(req, res) {
   res.render('customer/auth/login');
 }
 
-async function login(req, res) {
+async function login(req, res, next) {
   const user = new User(req.body.email, req.body.password);
-  const existingUser = await user.getUserWithSameEmail();
+  let existingUser;
+  try {
+    existingUser = await user.getUserWithSameEmail();
+  } catch (error) {
+    next(error);
+    return;
+  }
 
   if (!existingUser) {
     res.redirect('/login');
