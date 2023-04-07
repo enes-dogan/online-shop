6cb6f6e7f32d05e8c07e410795d51fd94951aee6
Enes-Dogan <enesdogan210@gmail.com>
2023-04-07 23:51:28 +0300

    showing nav items based on logged in or admin & styling

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ const baseRoutes = require('./routes/base.routes');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // path views is folder contains ejs
-
+app.set('views', path.join(__dirname, 'views'));
+// path views is folder contains ejs
 app.use(express.static('public')); // public folder serves staticly
-app.use(express.urlencoded({ extended: false })); // for handling data attached to request
-
+app.use(express.urlencoded({ extended: false }));
+// for handling data attached to request
 const sessionConfig = createSessionConfig();
 
 app.use(expressSession(sessionConfig));
