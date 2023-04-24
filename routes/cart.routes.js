50e0eb9b4303bbf0930177fdc43dfbb346aa0d41
Enes-Dogan <enesdogan210@gmail.com>
2023-04-24 19:37:57 +0300

    cart page view created and filled with data accessible via locals and styled

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -4,6 +4,9 @@ const cartController = require('../controllers/cart.controller');
 
 const router = express.Router();
 
+router.get('/', cartController.getCart); // /cart/items
+
+
 router.post('/items', cartController.addCartItem); // /cart/items
 
 module.exports = router;
