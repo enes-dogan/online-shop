436f3f2f4f945a2be825414f30d9b168feb4d42a
Enes-Dogan <enesdogan210@gmail.com>
2023-04-24 18:35:38 +0300

    cart logic and storing using with sessions added, cart badge refreshes upon product add button using ajax

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const protectRoutesMiddleware = require('./middlewares/protect-routes');
+const cartMiddleware = require('./middlewares/cart');
 const authRoutes = require('./routes/auth.routes');
 const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
 const adminRoutes = require('./routes/admin.routes');
+const cartRoutes = require('./routes/cart.routes');
 
 const app = express();
 
@@ -22,12 +24,15 @@ app.set('views', path.join(__dirname, 'views'));
 // path views is folder contains ejs
 app.use(express.static('public')); // public folder serves staticly
 app.use('/products/assets', express.static('product-data')); // product-data folder serves staticly
-app.use(express.urlencoded({ extended: false }));
-// for handling data attached to request
+app.use(express.urlencoded({ extended: false })); // for handling data attached to request
+app.use(express.json());
+
 const sessionConfig = createSessionConfig();
 
 app.use(expressSession(sessionConfig));
 app.use(csrf());
+
+app.use(cartMiddleware);
 
 app.use(addCsrfTokenMiddleware);
 app.use(checkAuthStatusMiddleware);
@@ -35,6 +40,7 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
+app.use('/cart', cartRoutes);
 app.use(protectRoutesMiddleware);
 app.use('/admin', adminRoutes); // only urls start with /admin will handled by this middleware
 
